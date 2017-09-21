facafd0f90b03a1127cac007ac493e4dd3ee0c28
bursalma <bursalma@gmail.com>
2017-09-21 14:27:31 -0400

    Eighth

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+
+    static public int score = 1000;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        GUIText gt = this.GetComponent<GUIText>();
+        gt.text = "High Score: " + score;
+	}
+}
